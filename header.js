b9f9a00442d3fce35c63ecd55446a1cdc6c3bc37
Jyotishman Goswami <jyotishman2468@gmail.com>
2022-09-06 11:31:41 +0530

    header section

---
+++ header.js
@@ -0,0 +1,15 @@
+let header_Content = document.getElementById("header-Template");
+
+header_Content.innerHTML =  `<header>
+        <nav>
+            <div class="logo">SPA</div>
+            <div class="links">
+                <ul>
+                <li><a href="#" id="home">Home</a></li>
+                <li><a href="#/about">About</a></li>
+                <li><a href="#/contact">Contact</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    </header>`
