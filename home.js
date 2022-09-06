bf3d894d17e053669c6fdbfa5c9e3c28b61816f1
Jyotishman Goswami <jyotishman2468@gmail.com>
2022-09-06 11:32:06 +0530

    home page

---
+++ home.js
@@ -0,0 +1,19 @@
+ export let HomeScreen = {
+        render:()=>{
+            
+         return   `<div class="Content" id="Cont">
+            <h1>Homes</h1></div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, velit at corporis, nostrum in facilis fugiat cumque nihil, deleniti quidem quas quos quod et ipsum enim id. Eligendi accusantium, fugit earum laudantium sunt molestiae dignissimos voluptatum eius sed soluta, obcaecati quos fugiat, dolor cum accusamus? Unde sed aperiam rem modi?</p>
+
+         `
+        }
+
+      }
+
+      
+
+
+
+
+ 
+ 
